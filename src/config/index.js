bf75b2be4ac3d328996d63b2f751471ba91c5545
shixl <shixl@dist.com.cn>
2024-07-16 16:50:35 +0800

    docker部署修改

--- src/config/index.js
+++ src/config/index.js
@@ -14,7 +14,7 @@ const EnvConfig = {
       "https://www.fastmock.site/mock/4f8a76c109fe7d95e131ee5a72d23dff/api",
   },
   prod: {
-    baseApi: "//futurefe.com/api",
+    baseApi: "/api",
     mockApi:
       "https://www.fastmock.site/mock/4f8a76c109fe7d95e131ee5a72d23dff/api",
   },
